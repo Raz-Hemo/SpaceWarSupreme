b812d50ee3d74ebbb4a763d0e7432a318242a186
Raz <razhemo1337@gmail.com>
2020-02-01 18:13:30 +0200

    cleanup

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -1,4 +1,4 @@
 pub const CONFIG_FILE_PATH: &str = "./config.ini";
-pub const ICON_PATH: &str = "./resources/icon.png";
+pub const ICON_PATH: &str = "./resources/icon.ico";
 
 pub const WINDOW_NAME: &str = "Space War Supreme!";
