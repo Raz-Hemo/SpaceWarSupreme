2ae037b6a4d043dba7896082be77fe21572f676a
Raz <razhemo1337@gmail.com>
2020-02-28 23:16:30 +0200

    audio mostly done

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,15 +1,84 @@
-pub type SoundID = u32;
-pub type SoundEventQueue = std::sync::mpsc::Receiver<SoundID>;
+pub enum SoundEvent {
+    Play(ArcSoundBank, SoundID),
+    AcquireDevice,
+    DestroyDevice,
+}
 
-pub fn audio_worker_thread(queue: SoundEventQueue) {
+pub type SoundID = String;
+pub type SoundEventQueueRx = std::sync::mpsc::Receiver<SoundEvent>;
+pub type SoundEventQueueTx = std::sync::mpsc::Sender<SoundEvent>;
+pub type SoundBank = std::collections::HashMap<SoundID, Sound>;
+pub type ArcSoundBank = std::sync::Arc<SoundBank>;
+
+pub fn new_soundbank() -> SoundBank {
+    std::collections::HashMap::new()
+}
+
+fn try_acquire_audio_device() -> Option<rodio::Device>{
     if let Some(device) = rodio::default_output_device() {
-        loop {
-            match queue.recv() {
-                Ok(id) => rodio::play_raw(&device, some_way_to_get_source_from_id(id)),
-                Err(e) => break,
-            }
-        }
+        Some(device)
     } else {
         crate::log::error("Could not find a sound device for output!");
+        None
+    }
+}
+
+pub fn audio_worker_thread(queue: SoundEventQueueRx) {
+    // For convert_samples()
+    use rodio::source::Source;
+    let mut device = try_acquire_audio_device();
+
+    loop {
+        match queue.recv() {
+            Ok(SoundEvent::Play(assets, id)) => {
+                if assets.contains_key(&id) && device.is_some() { 
+                    rodio::play_raw(device.as_ref().unwrap(), assets[&id].decoder().convert_samples()) 
+                }
+            },
+            Ok(SoundEvent::AcquireDevice) => {
+                device = try_acquire_audio_device();
+            },
+            Ok(SoundEvent::DestroyDevice) => {
+                device = None
+            },
+            Err(_) => break,
+        }
+    }
+}
+
+use std::io;
+
+pub struct Sound {
+    samples: std::sync::Arc<Vec<u8>>,
+}
+
+impl std::convert::AsRef<[u8]> for Sound {
+    fn as_ref(&self) -> &[u8] {
+        &self.samples
+    }
+}
+
+impl Sound {
+    pub fn new(filename: &str) -> crate::utils::SWSResult<Sound> {
+        use std::io::Read;
+        use std::fs::File;
+
+        let file = File::open(filename);
+        if file.is_err() {
+            return Err(format!("Failed to open sound file {}", filename));
+        }
+        let mut file = file.unwrap();
+
+        let mut buf = Vec::new();
+        if file.read_to_end(&mut buf).is_err() {
+            return Err(format!("Failed to read sound file {}", filename));
+        }
+
+        Ok(Sound { samples: std::sync::Arc::new(buf) })
+    }
+    pub fn decoder(self: &Self) -> rodio::Decoder<io::Cursor<Sound>> {
+        rodio::Decoder::new(io::Cursor::new(
+            Sound { samples: self.samples.clone() }
+        )).unwrap()
     }
 }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,16 +1,76 @@
-use std::sync::mpsc::{channel, Sender, Receiver};
+use std::sync::mpsc::channel;
 
 pub struct Engine<'a> {
     pub input: crate::input::InputInfo<'a>,
     pub cfg: crate::config::Config,
-    pub audio: Sender<;
+    audio_sender: crate::audio::SoundEventQueueTx,
+    audio_assets: crate::audio::ArcSoundBank,
+    audio_thread: Option<std::thread::JoinHandle<()>>,
 }
 
 impl<'a> Engine<'a> {
     pub fn new() -> Engine<'a> {
+        let (audio_sender, audio_receiver) = channel();
+        let audio_assets = Engine::load_sounds();
+        let audio_thread = std::thread::Builder::new().name(String::from("spacewar_audio"))
+        .spawn(|| {
+            crate::audio::audio_worker_thread(audio_receiver);
+        });
+
+        if audio_thread.is_err() {
+            crate::log::error(&format!("Failed to create audio thread: {:?}", audio_thread));
+        }
+
         Engine {
             input: crate::input::InputInfo::new(),
             cfg: crate::config::Config::load(),
+            audio_sender: audio_sender,
+            audio_assets: audio_assets,
+            audio_thread: audio_thread.ok(),
+        }
+    }
+
+    fn load_sounds() -> crate::audio::ArcSoundBank {
+        let mut result = crate::audio::new_soundbank();
+
+        for f in crate::utils::get_files_with_extension_from(
+                crate::consts::SOUND_FOLDER_PATH, Vec::from(crate::consts::SUPPORTED_SOUND_EXTENSIONS)) {
+            if let Some(name) = f.file_stem() {
+                let filename = &String::from(f.to_string_lossy());
+                match crate::audio::Sound::new(filename) {
+                    Err(e) => crate::log::error(&e),
+                    Ok(s) => { result.insert(
+                        String::from(name.to_string_lossy()), 
+                        s);
+                    },
+                }
+            }
+        }
+
+        std::sync::Arc::new(result)
+    }
+
+    pub fn play_sound(&self, id: &str) {
+        if self.audio_assets.contains_key(id) {
+            if let Err(e) = self.audio_sender.send(
+                crate::audio::SoundEvent::Play(self.audio_assets.clone(), String::from(id))
+            ) {
+                crate::log::warning(&format!("Failed to send sound to worker thread: {}", e));
+            }
+        } else {
+            crate::log::error(&format!("No such sound {}", id));
+        }
+    }
+
+    pub fn acquire_audio_device(&self) {
+        if let Err(e) = self.audio_sender.send(crate::audio::SoundEvent::AcquireDevice) {
+            crate::log::error(&format!("Send acquire message to audio thread failed: {}", e))
+        }
+    }
+
+    pub fn destroy_audio_device(&self) {
+        if let Err(e) = self.audio_sender.send(crate::audio::SoundEvent::DestroyDevice) {
+            crate::log::error(&format!("Send destroy message to audio thread failed: {}", e))
         }
     }
 }
