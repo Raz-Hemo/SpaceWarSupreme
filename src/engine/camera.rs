26bf0f1615f77a8b5ca0019e6a475dae8dfd23a6
Raz <razhemo1337@gmail.com>
2020-03-28 23:51:02 +0300

    y axis now goes up

--- src/engine/camera.rs
+++ src/engine/camera.rs
@@ -17,7 +17,7 @@ impl Camera {
     }
 
     pub fn get_view_matrix(&self) -> Matrix4<f32> {
-        Matrix4::look_at(self.pos, self.look_at, Vector3::new(0.0, 1.0, 0.0))
+        Matrix4::look_at(self.pos, self.look_at, Vector3::new(0.0, -1.0, 0.0))
     }
 
     pub fn get_projection_matrix(&self) -> Matrix4<f32> {
